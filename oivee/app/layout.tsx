d9a6cefb91cdda70abb4028ae9ca9cb7aa65d377
Oishik Samanta <clg.oishiksamanta@gmail.com>
2026-04-27 19:05:11 +0530

    favicon to homepage

--- oivee/app/layout.tsx
+++ oivee/app/layout.tsx
@@ -71,6 +71,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={dmSans.variable}>
       <head>
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         {gaId && (
           <>
             <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
